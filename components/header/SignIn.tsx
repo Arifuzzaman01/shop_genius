d3019f8c6eeba647f9e9c74d4f7a7429204f8762
Arifuzzaman01 <arifuzzamanar29@gmail.com>
2025-12-05 18:57:09 +0600

    solve deploy related issue

--- components/header/SignIn.tsx
+++ components/header/SignIn.tsx
@@ -2,14 +2,13 @@
 import React from 'react';
 import UserProfileDropdown from './UserProfileDropdown';
 import { signIn, useSession } from 'next-auth/react';
-import { redirect } from 'next/navigation';
 
 const SignIn = () => {
 const {data: session} = useSession()
   return (
     <div>
       {
-      session ? <UserProfileDropdown /> : <button onClick={() => redirect("/auth/signin") } className='px-2.5 py-1.5 bg-shop_light_green hover:bg-shop_btn_dark_green text-white rounded-sm'>Sign In</button>
+      session ? <UserProfileDropdown /> : <button onClick={() => signIn() } className='px-2.5 py-1.5 bg-shop_light_green hover:bg-shop_btn_dark_green text-white rounded-sm'>Sign In</button>
       }
     </div>
   );
